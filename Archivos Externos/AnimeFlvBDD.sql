aead3b6b746eac0f749943a1933d62ce2f3d5255
Alejandro Asencio Montes <63419024+NsoEnigma@users.noreply.github.com>
2022-05-30 14:01:10 +0200

    base de datos actualizada

--- Archivos Externos/AnimeFlvBDD.sql
+++ Archivos Externos/AnimeFlvBDD.sql
@@ -9,12 +9,53 @@ create table usuario(
     fechaNacimiento date
 );
 
-create table animes(
+create table anime(
 	nombre varchar(200) primary key,
+    portada blob,
+    descripcion varchar(200),
     foreign key (temporada) references temporadas(numeroTemporada)
 );
 
 create table temporadas(
-	nombre varchar(200) primary key,
-	numeroTemporada int(3)
+	nombre varchar(200),
+	numeroTemporada int(3),
+    usuario varchar(200),
+    fechaEstreno date,
+    descripcion varchar(200),
+	portada blob,
+    anime varchar(200),
+    foreign key (anime) references anime(nombre),
+    foreign key (usuario) references usuario(nombre),
+    primary key(numeroTemporada,anime)
+);
+
+create table capitulo(
+	nombre varchar(200),
+    link varchar(200),
+    numeroCapitulo int(3),
+    descripcion varchar(200),
+    portada blob,
+    temporada varchar(200),
+    foreign key (anime) references anime(nombre),
+    foreign key (usuario) references usuario(nombre),
+    primary key(numeroCapitulo,anime)
+    
+);
+
+create table ova(
+	nombre varchar(200),
+    link varchar(200),
+    numeroOva int(3),
+    descripcion varchar(200),
+    portada blob,
+    temporada varchar(200),
+    foreign key (anime) references anime(nombre),
+    foreign key (usuario) references usuario(nombre),
+    primary key(numeroOva,anime)
+    
+);
+
+create table noticia(
+	nombre varchar(200),
+	descripcion varchar(200)
 );
